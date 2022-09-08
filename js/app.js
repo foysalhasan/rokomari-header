f2555f3fa0efb3acd51701c9d4205a41fa749648
Foysal Hasan <foysalhasan499@gmail.com>
2022-09-08 09:55:34 +0600

    change

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const header = document.querySelector('.header')
+const header = document.querySelector('.header-top')
+const mobileHeader = document.querySelector('.mobile-header')
 
 window.addEventListener('scroll', function () {
   const top = window.scrollY
@@ -6,5 +7,14 @@ window.addEventListener('scroll', function () {
     header.classList.add('active')
   } else {
     header.classList.remove('active')
+  }
+})
+
+window.addEventListener('scroll', function () {
+  const top = window.scrollY
+  if (top > 200) {
+    mobileHeader.classList.add('active')
+  } else {
+    mobileHeader.classList.remove('active')
   }
 })
